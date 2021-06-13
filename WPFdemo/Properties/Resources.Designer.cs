d48d924341c21f6c03e869531a8ec380cbb57ff8
HusrFX <hotamovhusrav@gmail.com>
2021-06-13 18:14:27 +0300

    s2

--- WPFdemo/Properties/Resources.Designer.cs
+++ WPFdemo/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace WPFdemo.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Icon, аналогичного (Значок).
+        /// </summary>
+        internal static System.Drawing.Icon beauty_logo1 {
+            get {
+                object obj = ResourceManager.GetObject("beauty_logo1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
